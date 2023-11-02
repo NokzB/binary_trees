89fd572252e0af60a94252ff410d298091adcbbd
Nokuphila <nnblb58@gmail.com>
2023-11-02 04:58:17 -0700

    corrected pointer value

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -7,6 +7,7 @@
  * Return: the pointer to the left node
  *	Otherwise - NULL
  */
+
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
@@ -15,13 +16,14 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		return (NULL);
 
 	new_node = malloc(sizeof(binary_tree_t))
-	if (left_node == NULL)
+	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = value;
 	new_node->parent = parent;
 	new_node->left = NULL;
 	new_node->right = NULL;
+
 	if (parent->left != NULL)
 	{
 		new_node->left = parent->left;
