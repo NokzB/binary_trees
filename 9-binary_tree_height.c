f1944415841f2ca9fb86eb84125b1bc42f322345
Nokuphila Bhembe <nnblb58@gmail.com>
2023-11-02 19:24:07 +0200

    update

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -11,7 +11,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 {
 	if (tree)
 	{
-		size_t l = 0, r = 0;
+		size_t lft = 0, rgt = 0;
 
 		lft = tree->left ? 1 + binary_tree_height(tree->left) : 0;
 		rgt = tree->right ? 1 + binary_tree_height(tree->right) : 0;
