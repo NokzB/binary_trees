db2c0e49d049379488b02eaaeae702fa3363e43a
Nokuphila <nnblb58@gmail.com>
2023-11-02 04:54:23 -0700

    initial commit for task 1

--- binary_trees.h
+++ binary_trees.h
@@ -41,4 +41,5 @@ typedef struct binary_tree_s heap_t;
 /* prototypes */
 
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 #endif /* BINARY_TREES_ H*/
